1bd7a2fa349cae34721f5af79a5a70867fa97829
José Neto <sputinykster@gmail.com>
2021-06-02 12:21:08 +0100

    setup tests

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,16 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+const babelPlugins = isProd
+  ? [['react-remove-properties', { properties: ['data-testid'] }]]
+  : []
+
 module.exports = {
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
+  },
+  babel: {
+    plugins: babelPlugins,
   },
 }
